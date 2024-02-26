96794c7d3bc293976ebbcf595b77db212ce4fde4
nqbao47 <baonguyengau14@gmail.com>
2024-02-26 19:35:20 +0700

    Install MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@mui/material/'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleIncrease = () => {
+    setCount(count + 1)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p>Brian Nè ^^</p>
+      <h1>{count}</h1>
+      <Button variant="contained" onClick={handleIncrease}>
+        Ấn dô nè
+      </Button>
     </>
   )
 }
