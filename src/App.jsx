b5404d75fc4aceb321054074ac2052564eac4745
nqbao47 <baonguyengau14@gmail.com>
2024-02-26 20:40:23 +0700

    Add toggle mode logic to change dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import { useState } from 'react'
 import { Button } from '@mui/material/'
 
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+// const theme = extendTheme({
+//   // ...your custom theme
+// })
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +29,8 @@ function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <p>Brian Nè ^^</p>
       <h1>{count}</h1>
       <Button variant="contained" onClick={handleIncrease}>
