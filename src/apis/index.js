b1a05980eff7c06fbb9929363ee988c776220944
nqbao47 <baonguyengau14@gmail.com>
2024-03-20 11:18:00 +0700

    Merge 2 Apis create createNewColumn & createNewCard into UI

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
 import { API_ROOT } from '~/utils/constants'
 
+/** Boards */
 export const fetchBoardDetailsAPI = async (boardId) => {
   const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
-
   // axios: trả kết quả về qua property của nó là data
+  return response.data
+}
+
+/** Columns */
+export const createNewColumnsAPI = async (newColumnData) => {
+  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
+  return response.data
+}
+
+/** Cards */
+export const createNewCardAPI = async (newCardData) => {
+  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
   return response.data
 }
