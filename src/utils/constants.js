0167455666c5f609b1205a0445af35c2ee5d34d6
nqbao47 <baonguyengau14@gmail.com>
2024-03-23 05:31:31 +0700

    Fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,14 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-backend-yxfm.onrender.com'
+let apiRoot = ''
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-backend-yxfm.onrender.com'
+}
+
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
